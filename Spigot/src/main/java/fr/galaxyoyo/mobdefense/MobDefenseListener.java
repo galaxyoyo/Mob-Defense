99ec4148efe38817035dd74abf6c8084cc6565fa
galaxyoyo <yohann.danello@gmail.com>
2016-06-17 18:20:54 +0200

    Block placing/breaking fixed

--- Spigot/src/main/java/fr/galaxyoyo/mobdefense/MobDefenseListener.java
+++ Spigot/src/main/java/fr/galaxyoyo/mobdefense/MobDefenseListener.java
@@ -13,7 +13,6 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.*;
@@ -157,7 +156,7 @@ public class MobDefenseListener implements Listener
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event)
 	{
-		if (event.getAction() != Action.RIGHT_CLICK_BLOCK || event.getClickedBlock().getType() != Material.DISPENSER)
+		if (!event.getAction().name().contains("BLOCK") || event.getClickedBlock().getType() != Material.DISPENSER)
 			event.setCancelled(true);
 	}
 
